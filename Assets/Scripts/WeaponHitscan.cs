65aed4ab13952fafb3cbd51ed38a7a5a5702ae9b
MatK <mathew.kostrzewa@ontariotechu.net>
2020-07-29 00:15:06 -0400

    Added health to player and headshot detection

--- Assets/Scripts/WeaponHitscan.cs
+++ Assets/Scripts/WeaponHitscan.cs
@@ -70,9 +70,7 @@ public class WeaponHitscan : MonoBehaviour
         {
             bulletTrailEndPos = hit.point;
 
-            Health objectHitHealth;
-            if (hit.transform.TryGetComponent<Health>(out objectHitHealth))
-                objectHitHealth.TakeDamage(damagePerBullet);
+            ProcessWeaponHit(hit.transform.gameObject);
         }
         else //no hit
         {
@@ -84,12 +82,36 @@ public class WeaponHitscan : MonoBehaviour
 
         //spawn first person bullet trail
         //if the hit was super close, the trail would look weird so don't draw it
-        if (bulletTrailFPP && Vector3.Distance(muzzleTransformFPP.position, hit.point) > MIN_DISTANCE_FOR_BULLET_TRAIL)
+        if (bulletTrailFPP && Vector3.Distance(muzzleTransformFPP.position, bulletTrailEndPos) > MIN_DISTANCE_FOR_BULLET_TRAIL)
             DrawBulletTrail(muzzleWorldPosFPP, bulletTrailEndPos, bulletTrailFPP, m_bulletTrailLayerNumFPP);
 
         //spawn third person bullet trail
         if (bulletTrailTPP)
             DrawBulletTrail(muzzleTransformTPP.position, bulletTrailEndPos, bulletTrailTPP, m_bulletTrailLayerNumTPP);
+    }
+
+    void ProcessWeaponHit(GameObject a_hitObject)
+    {
+        Health objectHitHealth;
+
+        //try to find a health component
+        if (!a_hitObject.TryGetComponent<Health>(out objectHitHealth))
+        {
+            GameObject objectWithHealth = MyHelper.FindFirstParentWithComponent(a_hitObject, typeof(Health)); //go up the hierarchy and see if any parent has a health component
+            if (objectWithHealth)
+                objectHitHealth = objectWithHealth.GetComponent<Health>();
+        }
+
+        //if the object hit has health, deal damage
+        if (objectHitHealth)
+        {
+            float damageToInflict = damagePerBullet;
+            //check for headshot
+            if (a_hitObject.TryGetComponent<Head>(out var playerHead))
+                damageToInflict *= headshotMultiplier;
+
+            objectHitHealth.TakeDamage(damageToInflict);
+        }
     }
 
     void DrawBulletTrail(Vector3 a_startPos, Vector3 a_endPos, LineRenderer a_bulletTrailLR, int a_bulletTrailLayer)
