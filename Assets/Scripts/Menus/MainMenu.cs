e07a9069fc2a46b051a37a343ebbe5fb5533876a
MatK <mathew.kostrzewa@ontariotechu.net>
2020-08-03 14:32:41 -0400

    Added main menu with very basic host/join game functionality

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using Bolt.Matchmaking;
+using Bolt;
+using UnityEngine;
+using UdpKit;
+using System;
+
+public class MainMenu : Bolt.GlobalEventListener
+{
+    public void StartServer()
+    {
+        BoltLauncher.StartServer();
+    }
+
+    public void StartClient()
+    {
+        BoltLauncher.StartClient();
+    }
+
+    public override void BoltStartDone()
+    {
+        if (BoltNetwork.IsServer)
+        {
+            string matchName = Guid.NewGuid().ToString();
+
+            BoltMatchmaking.CreateSession(
+                sessionID: matchName,
+                sceneToLoad: "MainMenu"
+                );
+        }
+    }
+
+    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+    {
+        Debug.LogFormat("Session list updated: {0} total sessions", sessionList.Count);
+
+        foreach (var session in sessionList)
+        {
+            UdpSession photonSession = session.Value as UdpSession;
+
+            if (photonSession.Source == UdpSessionSource.Photon)
+            {
+                BoltMatchmaking.JoinSession(photonSession);
+            }
+        }
+    }
+}
